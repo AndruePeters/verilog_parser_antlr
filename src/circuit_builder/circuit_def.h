392c6330ce15387e601604b41a1a1ebf1b02126a
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2020-08-19 18:43:43 -0400

    Separated implementation Added VerilogPrimitive Added "addPrimitive()" method to ModuleDefinition

--- src/circuit_builder/circuit_def.h
+++ src/circuit_builder/circuit_def.h
@@ -8,8 +8,7 @@
 #include <string>
 #include <vector>
 
-namespace Circuit
-{
+namespace Circuit {
 /// Direction of a port
 enum struct PortDir : short {
     inout,
@@ -22,8 +21,8 @@ enum struct PortDir : short {
 ///                 this will connect the port to an internal net or something
 /// \param external is an external connection from the module's perspective
 ///                 this will connect the port to an instance outside of the module
-enum struct PortView: short {
-    internal, 
+enum struct PortView : short {
+    internal,
     external
 };
 
@@ -55,14 +54,15 @@ class Port {
 public:
 
     /// Build a port object
-    Port(const std::string portName, const BitWidth bitWidth): 
-        name_(std::move(portName)), width_(std::move(bitWidth))  {}
+    Port(const std::string portName, const BitWidth bitWidth) :
+            name_(std::move(portName)), width_(std::move(bitWidth)) {}
 
     /// Describes a connection for a Port
     /// Valid value for connectionType are "net", "port", "reg"
     /// connectionIndex should be set if using a bus
-    void addConnection(const std::string connectionType, const std::string connectionName, const unsigned connectionIndex = 0) {
-        connections_.push_back( {std::move(connectionType), std::move(connectionName), connectionIndex} );
+    void addConnection(const std::string connectionType, const std::string connectionName,
+                       const unsigned connectionIndex = 0) {
+        connections_.push_back({std::move(connectionType), std::move(connectionName), connectionIndex});
     }
 
     /// Sets the direction for a port
@@ -92,20 +92,24 @@ private:
 /// Describes how a net should be constructed
 class Net {
 public:
-    
+
     /// Build a net object
-    Net(const std::string netType, const std::string netName, const BitWidth bitWidth = {0,0}): 
-         type_(std::move(netType)), name_(std::move(netName)), width_(std::move(bitWidth))  {}
+    Net(const std::string netType, const std::string netName, const BitWidth bitWidth = {0, 0}) :
+            type_(std::move(netType)), name_(std::move(netName)), width_(std::move(bitWidth)) {}
 
     /// Describes a net connection
     /// Valid value for connectionType are "instance", "port", "reg"
-    void addConnection(const std::string connectionType, const std::string connectionName, const unsigned connectionIndex = 0) {
-        connections_.push_back( {std::move(connectionType), std::move(connectionName), connectionIndex} );
+    void addConnection(const std::string connectionType, const std::string connectionName,
+                       const unsigned connectionIndex = 0) {
+        connections_.push_back({std::move(connectionType), std::move(connectionName), connectionIndex});
     }
 
     const std::string& name() const { return name_; }
+
     const std::string& type() const { return type_; }
-    const BitWidth&    width()   const { return width_;}
+
+    const BitWidth& width() const { return width_; }
+
 private:
     std::string type_;  ///< net type
     std::string name_;  ///< net name
@@ -114,32 +118,43 @@ private:
 };
 
 /// Describes an instantiation of a module
-class InstanceDefinition  {
+class InstanceDefinition {
 public:
     /// Buiild an instance definition
     /// \p type the type of instance, typically a module name
     /// \p name name of the instance
-    InstanceDefinition(std::string type, std::string name): type_(std::move(type)), name_(std::move(name)) { }
-    
+    InstanceDefinition(std::string type, std::string name) : type_(std::move(type)), name_(std::move(name)) {}
+
     /// Add a port to an instance
     /// Throws exception of a port with the same name already exists
     Port& addPort(const Port portDef);
 
     const std::string& name() const { return name_; }
+
     const std::string& type() const { return type_; }
+
 private:
     std::string type_;
-    std::string  name_;
+    std::string name_;
     std::vector<Port> ports_;
 };
 
 /// Models gates in Verilog: buf, xor, or, not, and
-class VerilogPrimitiveDefinition {
+class VerilogPrimitive {
 public:
-    VerilogPrimitiveDefinition(const std::string name): name_(std::move(name)) {}
+    VerilogPrimitive(const std::string type, const std::string name) : type_(std::move(type)), name_(std::move(name)) {}
+
+    void addInput(const std::string input)      { gateIn_.push_back(std::move(input)); }
+
+    void addOutput(const std::string output)    { gateOut_.push_back(std::move(output)); }
+
+    const std::string& name() const { return name_; }
+    const std::string& type() const { return type_; }
+
 private:
-    std::string name_; 
-    std::string gateOut_;
+    std::string type_;
+    std::string name_;
+    std::vector<std::string> gateOut_;
     std::vector<std::string> gateIn_;
 };
 
@@ -153,7 +168,7 @@ private:
 /// \p instances     - vector of all instances within the module
 class ModuleDefinition {
 public:
-    ModuleDefinition(const std::string name) : name_(std::move(name)) { }
+    ModuleDefinition(const std::string name) : name_(std::move(name)) {}
 
     /// Add a port to an instance definition
     /// Throws exception of port already exists as it's a sign of a malformed Verilog file
@@ -177,12 +192,12 @@ public:
     /// Adds a gate
     /// \return Address of newly added gate
     /// Throws exception of an gate with the same name already exists within this module
-    InstanceDefinition& addGate(const InstanceDefinition gateInst);
+    VerilogPrimitive& addPrimitive(const VerilogPrimitive gateInst);
 
 
     /// Finds a gate by name
     /// \return pointer to instance if it exists; otherwise nullptr
-    InstanceDefinition* findGate(const std::string& gateName);
+    VerilogPrimitive* findPrimitive(const std::string& primitiveName);
 
     /// Adds a net
     /// \return Reference to newly added net
@@ -200,7 +215,7 @@ private:
     std::string name_;
     std::vector<Port> ports_;
     std::vector<InstanceDefinition> instances_;
-    std::vector<InstanceDefinition> gates_;
+    std::vector<VerilogPrimitive> verilogPrimitives_;
     std::vector<Net> nets_;
 };
 
